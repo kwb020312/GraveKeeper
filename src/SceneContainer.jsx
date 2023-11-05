dda68916faca94c59fc7b8a5a510a65a76cc7acd
김우빈 <kwb020312@naver.com>
2023-11-05 22:51:13 +0900

    Feat: BoilerPlate

---
+++ src/SceneContainer.jsx
@@ -0,0 +1,24 @@
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import { Suspense } from "react";
+
+const SceneContainer = () => {
+  return (
+    <Suspense fallback={null}>
+      <Environment background={"only"} files={"/textures/bg.hdr"} />
+      <Environment background={false} files={"/textures/envmap.hdr"} />
+
+      <PerspectiveCamera
+        makeDefault
+        fov={50}
+        position={[-1.75, 10.85, 20.35]}
+      />
+      <OrbitControls target={[1, 5, 0]} maxPolarAngle={Math.PI * 0.5} />
+    </Suspense>
+  );
+};
+
+export default SceneContainer;
