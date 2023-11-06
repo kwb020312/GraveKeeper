415b9702e966e1df12740ea5c9f3c9a50a2eb754
김우빈 <kwb020312@naver.com>
2023-11-06 22:06:09 +0900

    Feat: ts -> js transfer

--- src/SceneContainer.jsx
+++ src/SceneContainer.jsx
@@ -4,6 +4,7 @@ import {
   PerspectiveCamera,
 } from "@react-three/drei";
 import { Suspense } from "react";
+import FloatingIsland from "./FloatingIsland";
 
 const SceneContainer = () => {
   return (
@@ -17,6 +18,8 @@ const SceneContainer = () => {
         position={[-1.75, 10.85, 20.35]}
       />
       <OrbitControls target={[1, 5, 0]} maxPolarAngle={Math.PI * 0.5} />
+
+      <FloatingIsland />
     </Suspense>
   );
 };
